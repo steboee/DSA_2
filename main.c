01718efadfec463d64daf7f46ea57c3f5d07dc6a
Ctibor Kovalčík <ckkoval33@gmail.com>
2021-03-19 12:21:09 +0100

    Just tryin to do smth.

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+}NODE;
+
+
+
+int search(){
+
+}
+
+
+NODE* make_new_node(int data){
+    NODE* new = (NODE*)malloc(sizeof(NODE));
+    new->data = data;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+
+
+}
+
+
+
+int insert(NODE** bunka,int data){
+    if (*bunka == NULL){
+        *bunka = make_new_node(data);
+    }
+
+
+}
+
+
+
+
+
+
 
 int main() {
     printf("Hello, World!\n");
+
+    NODE* root=NULL;
+
+    insert(&root,50);
+
     return 0;
 }
